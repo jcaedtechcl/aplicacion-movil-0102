437a2c9f85c688c4e9e0ffb69a7e3915531da93e
jca.edtech.cl <jca.edtech.cl@gmail.com>
2026-06-21 22:56:57 -0400

    first

--- app/src/main/java/com/example/aplicacion0102/SiguientePasoActivity.java
+++ app/src/main/java/com/example/aplicacion0102/SiguientePasoActivity.java
@@ -1,6 +1,7 @@
 package com.example.aplicacion0102;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,9 +13,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SiguientePasoActivity extends AppCompatActivity {
 
+    private static final String TAG = "SiguientePasoActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_siguiente_paso);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -35,5 +39,41 @@ public class SiguientePasoActivity extends AppCompatActivity {
 
         Button btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(v -> finish());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 }
