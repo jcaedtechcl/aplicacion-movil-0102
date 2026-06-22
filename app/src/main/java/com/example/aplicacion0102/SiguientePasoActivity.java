c8cb7c3282f962d8f4847d1f1d9ce461aba04cee
jca.edtech.cl <jca.edtech.cl@gmail.com>
2026-06-21 21:17:18 -0400

    first

---
+++ app/src/main/java/com/example/aplicacion0102/SiguientePasoActivity.java
@@ -0,0 +1,35 @@
+package com.example.aplicacion0102;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SiguientePasoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_siguiente_paso);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView textViewResult = findViewById(R.id.textViewResult);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("userInput");
+            if (value != null) {
+                textViewResult.setText(value);
+            }
+        }
+    }
+}
